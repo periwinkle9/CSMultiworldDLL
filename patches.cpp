f638f590190ea48edda60b3e4e47643735b14150
periwinkle <generic.username.625.125.25.5@gmail.com>
2023-11-10 12:40:23 -0500

    Apply a patch to increase the vanilla TSC buffer size Temporary fix until the scripts themselves are revised to fit within the vanilla limit

--- patches.cpp
+++ patches.cpp
@@ -9,6 +9,9 @@ void applyPatches()
 	//patcher::setupPostInitHook();
 	patcher::setupCleanupHook();
 	applyGameHooks();
+	// Increase TSC buffer size (temporary fix until the scripts themselves are revised to fit within the vanilla limit)
+	const patcher::dword NewTSCBufferSize = 0xA000; // Double the vanilla limit should be enough
+	patcher::patchBytes(0x421545, reinterpret_cast<const patcher::byte*>(&NewTSCBufferSize), sizeof NewTSCBufferSize);
 }
 
 /* If patcher::setupPostInitHook() is called above, then this function will be called
